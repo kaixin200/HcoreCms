4a8fc50380f569ee0b89c56983ff2eeb14eca970
kaixin200 <henry365@126.com>
2020-03-05 21:30:48 +0800

    这是注释内容

---
+++ Sample05/Sample05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper; //“SqlConnection”未包含“Execute”的定义
+
+namespace Sample05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            test_insert();
+        }
+
+        /// <summary>
+        /// 测试插入单条数据
+        /// </summary>
+        static void test_insert()
+        {
+            var content = new Content
+            {
+                title = "标题1",
+                content = "内容1",
+
+            };
+            using (var conn = new SqlConnection("Data Source=.;User ID=sa;Password=123456;Initial Catalog=hcorecms;Pooling=true;Max Pool Size=100;"))
+            {
+                string sql_insert = @"INSERT INTO [Content]
+                (title, [content], status, add_time, modify_time)
+VALUES   (@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_insert：插入了{result}条数据！");
+            }
+        }
+    }
+}
